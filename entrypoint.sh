0a1a1e4431ebfdce064b2c42e59628f4e967c99d
Dviih <50752447+Dviih@users.noreply.github.com>
2021-02-06 12:39:06 -0300

    Added cloudflared

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 wget https://raw.githubusercontent.com/Dviih/dviih-gotty-cloudflared/main/gotty
 chmod +x gotty
 ./gotty -w bash &
+wget wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
+dpkg -i cloudflared-stable-linux-amd64.deb
 cloudflared run
