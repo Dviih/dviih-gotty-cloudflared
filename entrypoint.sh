c01c4f7b10e215f39a768a2ed8a22a8574ab5e75
Dviih <50752447+Dviih@users.noreply.github.com>
2021-04-01 09:15:55 -0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-wget https://raw.githubusercontent.com/Dviih/dviih-gotty-cloudflared/main/gotty
-chmod +x gotty
-./gotty -w bash &
-wget wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
-dpkg -i cloudflared-stable-linux-amd64.deb
-cloudflared run
